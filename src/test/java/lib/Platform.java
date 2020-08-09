709d120862d8a2cdf347e9e207399632a19c70ba
Amatlakhova <alina.matlakhova@gmail.com>
2020-08-09 20:51:35 +0300

    SearchPageObject for Mobile Web

--- src/test/java/lib/Platform.java
+++ src/test/java/lib/Platform.java
@@ -1,12 +1,15 @@
 package lib;
 
-import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.ios.IOSDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Platform {
 
@@ -35,6 +38,8 @@ public class Platform {
             return new AndroidDriver(URL, this.getAndroidDesiredCapabilities());
         } else if (this.isIOS()) {
             return new IOSDriver(URL, this.getIOSDesiredCapabilities());
+        }else if (this.isMW()) {
+            return new ChromeDriver(this.getMWChromeOptions());
         } else {
             throw new Exception("Cannot detect type of the driver. Platform value " + this.getPlatformVar());
         }
@@ -50,7 +55,7 @@ public class Platform {
         return isPlatform(PLATFORM_IOS);
     }
 
-    public boolean isMw()
+    public boolean isMW()
     {
         return isPlatform(PLATFORM_MOBILE_WEB);
     }
@@ -76,6 +81,23 @@ public class Platform {
         capabilities.setCapability("platformVersion","13.6");
         capabilities.setCapability("app","/Users/amatlakhova/Documents/Projects/mobile-automation-course/JavaAppiumAutomation/apks/Wikipedia.app");
         return capabilities;
+    }
+
+    private ChromeOptions getMWChromeOptions()
+    {
+        Map<String, Object> deviceMetrics = new HashMap<String, Object>();
+        deviceMetrics.put("width", 360);
+        deviceMetrics.put("height", 640);
+        deviceMetrics.put("pixelRatio", 3.0);
+
+        Map<String, Object> mobileEmulation = new HashMap<String, Object>();
+        mobileEmulation.put("deviceMetrics", deviceMetrics);
+        mobileEmulation.put("userAgent", "Mozilla/5.0 (Linux; Android 4.2.1; en-us; Nexus 5 Build/JOP40D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19");
+
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("window-size=340,640");
+
+        return chromeOptions;
     }
 
     private boolean isPlatform(String my_platform)
